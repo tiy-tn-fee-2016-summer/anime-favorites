86af71af142a7928aadb014d4210ec987072f31e
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-27 22:48:10 -0500

    User can register

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const User = use('App/Model/User');
+const Hash = use('Hash');
+
 class UserController {
   * create(request, response) {
     yield response.sendView('user.create');
@@ -7,17 +10,29 @@ class UserController {
 
   * store(request, response) {
     // Grab user input
+    const { email, password } = request.all();
     // Attempt to create a user
+    try {
+      yield User.create({ email, password: Hash.make(password) });
 
-    // Send back old input
-    yield request.withAll()
+      // Send success message
+      yield request.with({ success: 'User created!' })
+      // Make message and input last only one request
+      .flash();
+
+      // Redirect to login
+      response.redirect('/login');
+    } catch (e) {
+      // Send back old input
+      yield request.withAll()
       // Send error message
       .andWith({ error: 'The user could not be created!' })
       // Make message and input last only one request
       .flash();
 
-    // Redirect back
-    response.redirect('back');
+      // Redirect back
+      response.redirect('back');
+    }
   }
 }
 
